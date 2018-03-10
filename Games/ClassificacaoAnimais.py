1b8fa85c5c2b52eb23a449e42c3731236ac6edf6
Flavio Henrique Fonseca De Oliveira <flavio.fonseca@pucpr.edu.br>
2018-03-10 11:52:43 -0300

    #2

--- Games/ClassificacaoAnimais.py
+++ Games/ClassificacaoAnimais.py
@@ -3,5 +3,5 @@ import random
 class ClassificacaoAnimais(object):
 
 if __name__ == '__main__':
-    print("BEM VINDO AO JOGO DE CLASSIFICACAO DE ANIMAIS")
+    print("BEM VINDO AO JOGO DE CLASSIFICACAO DE ANIMAIS...")
     
